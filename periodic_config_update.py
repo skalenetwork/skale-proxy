08c5aa3c34c6d1864c4e529978504cde58cd6c25
kladko <13399135+kladkogex@users.noreply.github.com>
2021-06-04 21:26:08 +0300

    Added license

--- periodic_config_update.py
+++ periodic_config_update.py
@@ -20,37 +20,43 @@ def run(_command):
 
 
 def print_global_server_config(_f):
-    print("server {\n", _f)
-    print("	listen 80;\n", _f)
-    print("	root /usr/share/nginx/www;\n", _f)
-    print("	index index.php index.html index.htm;\n", _f)
-    print("	server_name localhost;\n", _f)
+    _f.write("server {\n")
+    _f.write("	listen 80;\n")
+    _f.write("	root /usr/share/nginx/www;\n")
+    _f.write("	index index.php index.html index.htm;\n")
+    _f.write("	server_name localhost;\n")
+
+
+def print_group_definition(_chain_info: ChainInfo, _f):
+    _f.write("upstream " + _chain_info.chain_name + " {\n")
+    for endpoint in _chain_info.list_of_domain_endpoints:
+        _f.write("   server " + endpoint + ";\n")
+    _f.write("}\n")
 
 
 def print_loadbalacing_config_for_chain(_chain_info: ChainInfo, _f):
-    print("upstream " + _chain_info.chain_name + " {\n", _f)
-    for endpoint in _chain_info.list_of_domain_endpoints:
-        print("   server " + endpoint + ";\n", _f)
-    print("}\n", _f)
-    print("	location /mainnet/" + _chain_info.chain_name + "{\n", _f)
-    print("	    proxy_http_version 1.1;\n", _f)
-    print("     proxy_pass http://" + _chain_info.chain_name + "/;\n", _f)
-    print("    }\n", _f)
+    _f.write("	location /mainnet/" + _chain_info.chain_name + " {\n")
+    _f.write("	      proxy_http_version 1.1;\n")
+    _f.write("	      proxy_pass http://" + _chain_info.chain_name + "/;\n")
+    _f.write("	    }\n")
 
 
 def print_config_file(_chain_infos: list):
     if os.path.exists(TMP_CONFIG_FILE):
         os.remove(TMP_CONFIG_FILE)
     with open(TMP_CONFIG_FILE, 'w') as f:
+        for chain_info in _chain_infos:
+            print_group_definition(chain_info, f)
         print_global_server_config(f)
         for chain_info in _chain_infos:
             print_loadbalacing_config_for_chain(chain_info, f)
-        print("}\n", f)
+        f.write("}\n")
+        f.close()
 
 
 def copy_config_file_if_modified():
     if (not path.exists(CONFIG_FILE)) or (not filecmp.cmp(CONFIG_FILE, TMP_CONFIG_FILE, shallow=False)):
-        print("New config file. Reloading server", file=sys.stderr)
+        print("New config file. Reloading server")
         os.remove(CONFIG_FILE)
         copyfile(TMP_CONFIG_FILE, CONFIG_FILE)
         run("/usr/sbin/nginx -s reload")
@@ -71,7 +77,7 @@ chain_info = ChainInfo("chain1", endpoints)
 chain_infos.append(chain_info)
 
 # let nginx start)
-time.sleep(10)
+time.sleep(30)
 
 while True:
     print("Checking Config file ")
