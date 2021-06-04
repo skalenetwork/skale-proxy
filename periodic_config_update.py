4f87107b420f405a952dc23b1d0cc01f3a73de48
kladko <13399135+kladkogex@users.noreply.github.com>
2021-06-04 16:32:38 +0300

    Added license

--- periodic_config_update.py
+++ periodic_config_update.py
@@ -12,24 +12,31 @@ CONFIG_FILE = "/etc/nginx/sites-available/default"
 TMP_CONFIG_FILE = "/tmp/tmp.config"
 
 
-endpoints = list()
+class ChainInfo:
+    def __init__(self, _chain_name, _list_of_domain_endpoints):
+        self.chain_name = _chain_name
+        self.list_of_domain_endpoints = _list_of_domain_endpoints
 
+
+endpoints = list()
 endpoints.append("testnet-16.skalenodes.com:10131")
 endpoints.append("testnet-15.skalenodes.com:10195")
 
+chain_info = ChainInfo("chain1", endpoints)
 
-def print_loadbalacing_group(_chain_name, _endpoints, _f):
-    print("upstream " + _chain_name + " {\n", _f)
-    for endpoint in _endpoints:
+
+def print_loadbalacing_group(_chain_info : ChainInfo, _f):
+    print("upstream " + _chain_info.chain_name + " {\n", _f)
+    for endpoint in _chain_info.list_of_domain_endpoints:
         print("   server " + endpoint + ";\n", _f)
     print("}\n", _f)
-    print("	location /mainnet/" + _chain_name + "{\n", _f)
+    print("	location /mainnet/" + _chain_info.chain_name + "{\n", _f)
     print("	    proxy_http_version 1.1;\n", _f)
-    print("     proxy_pass http://" + _chain_name + "/;\n", _f)
+    print("     proxy_pass http://" + _chain_info.chain_name + "/;\n", _f)
     print("    }\n", _f)
 
 
-def print_config_file(_endpoints):
+def print_config_file(_chain_info):
     if os.path.exists(TMP_CONFIG_FILE):
         os.remove(TMP_CONFIG_FILE)
     with open(TMP_CONFIG_FILE, 'w') as f:
@@ -38,7 +45,7 @@ def print_config_file(_endpoints):
         print("	root /usr/share/nginx/www;\n", f)
         print("	index index.php index.html index.htm;\n", f)
         print("	server_name localhost;\n", f)
-        print_loadbalacing_group("chain1", endpoints, f)
+        print_loadbalacing_group(_chain_info, f)
         print("}\n", f)
 
 
