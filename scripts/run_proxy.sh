63018cfbe187508f72272dfb4ee7a6ae97ab75de
Dmytro <dmitry.tkk@gmail.com>
2022-09-30 18:00:58 +0100

    Rename to proxy-ui

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -11,7 +11,7 @@ export EXPLORER_URL=$EXPLORER_URL
 export BASE_PROXY_URL=$BASE_PROXY_URL
 export ABIS_URL=$ABIS_URL
 
-bash $DIR/../proxy-ui/prepare_env_file.sh
+bash $DIR/../network-ui/prepare_env_file.sh
 
 cd $DIR/..
 docker-compose up --build -d
