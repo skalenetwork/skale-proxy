f103985c19dc10717fc5a4e9f5732cf4a2b69872
Dmytro <dmitry.tkk@gmail.com>
2024-02-05 16:54:52 +0000

    Remove network-ui submodule, update readme

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -5,7 +5,7 @@ set -e
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 : "${ENDPOINT?Need to set ENDPOINT}"
-: "${MANAGER_TAG?Need to set MANAGER_TAG}"
+: "${SERVER_NAME?Need to set SERVER_NAME}"
 
 cd $DIR/..
 docker-compose up --build -d
