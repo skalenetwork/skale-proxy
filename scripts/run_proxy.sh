11b7eb4270add456cb46f1c5b8b7b1b97310c726
Dmytro <dmitry.tkk@gmail.com>
2024-12-10 19:17:17 +0000

    Update compose cmd

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -7,4 +7,4 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 : "${ETH_ENDPOINT?Need to set ETH_ENDPOINT}"
 
 cd $DIR/..
-docker-compose up --build -d
+docker compose up --build -d
